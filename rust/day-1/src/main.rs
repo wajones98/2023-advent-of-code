008ce74db11449a7b5d622351fef94f42e80f767
wajones98 <will.jones@byzgen.com>
2023-12-01 12:15:40 +0000

    scaffold tests

--- rust/day-1/src/main.rs
+++ rust/day-1/src/main.rs
@@ -5,8 +5,14 @@ fn main() {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+    fn finds_two_digits() {}
+
+    #[test]
+    fn finds_two_digits_when_multiple_present() {}
+
+    #[test]
+    fn finds_two_digits_when_single_digit() {}
+
+    #[test]
+    fn correctly_sums_digits() {}
 }
