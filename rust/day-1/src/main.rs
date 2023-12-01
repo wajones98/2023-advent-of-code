26aa48b7cb7c81add6afa4a59fb54a874565076a
wajones98 <will.jones@byzgen.com>
2023-12-01 12:24:27 +0000

    finds_two_digits tests

--- rust/day-1/src/main.rs
+++ rust/day-1/src/main.rs
@@ -2,10 +2,36 @@ fn main() {
     println!("Hello, world!");
 }
 
+fn extract_digits(text: String) -> u64 {
+    0
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::extract_digits;
+
+    struct Test {
+        input: String,
+        output: u64,
+    }
+
     #[test]
-    fn finds_two_digits() {}
+    fn finds_two_digits() {
+        let tests = vec![
+            Test {
+                input: String::from("1abc2"),
+                output: 12,
+            },
+            Test {
+                input: String::from("pqr3stu8vwx"),
+                output: 38,
+            },
+        ];
+
+        for test in tests {
+            assert_eq!(extract_digits(test.input), test.output);
+        }
+    }
 
     #[test]
     fn finds_two_digits_when_multiple_present() {}
