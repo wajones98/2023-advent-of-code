b3f77aabed3825f747fa3a6cc5d064e95d118b81
Will <w.a.jones1998@gmail.com>
2023-12-02 13:46:52 +0000

    basic structs

--- rust/day-2/src/bin/part-1.rs
+++ rust/day-2/src/bin/part-1.rs
@@ -1,3 +1,36 @@
+struct RevealedSet {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+impl RevealedSet {
+    fn new(red: u32, green: u32, blue: u32) -> Self {
+       RevealedSet { red, green, blue } 
+    }
+}
+
+struct Game {
+    id: u32,
+    sets_revealed: Vec<RevealedSet>
+}
+
+impl Game {
+    fn new(id: u32, sets_revealed: Vec<RevealedSet>) -> Self {
+       Game {
+            id,
+            sets_revealed
+        } 
+    }
+    
+    //TODO: Parse game line from input
+    fn parse_string() -> Self {
+        let id = 1;
+        let sets_revealed = vec![];
+        Game::new(id, sets_revealed) 
+    }
+}
+
 fn main() {
     println!("Hello world!");
 }
