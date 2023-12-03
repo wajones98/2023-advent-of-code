1beb0606c0ef49e91b28c5cde729350d2b528e0a
Will <w.a.jones1998@gmail.com>
2023-12-03 14:18:37 +0000

    Test for parsing line

--- rust/day-3/src/bin/part-1.rs
+++ rust/day-3/src/bin/part-1.rs
@@ -2,11 +2,13 @@ fn main() {
     println!("Hello, world!");
 }
 
+#[derive(Debug, PartialEq)]
 enum PointType {
     Symbol(String),
     Digit(u32),
 }
 
+#[derive(Debug, PartialEq)]
 struct Point {
     x: u32,
     y: u32,
@@ -27,8 +29,14 @@ fn new_schematic(lines: Vec<&str>) -> Schematic {
     ] 
 }
 
+fn parse_schematic_line(line: &str) -> Vec<Point> {
+    vec![]
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::{Schematic, new_schematic, parse_schematic_line, Point, PointType};
+
     const TEST_ENGINE_SCHEMATIC: [&str; 10] = [
         "467..114..",
         "...*......",
@@ -44,7 +52,63 @@ mod tests {
 
 
     #[test]
-    fn should_parse_line() {}
+    fn should_parse_line() {
+        let line = TEST_ENGINE_SCHEMATIC[0];
+        let expected: Vec<Point> = vec![
+                Point {
+                    x: 0,
+                    y: 0,
+                    point_type: PointType::Symbol("4".to_string())
+                },
+                Point {
+                    x: 0,
+                    y: 0,
+                    point_type: PointType::Symbol("6".to_string())
+                },
+                Point {
+                    x: 0,
+                    y: 0,
+                    point_type: PointType::Symbol("7".to_string())
+                },
+                Point {
+                    x: 0,
+                    y: 0,
+                    point_type: PointType::Symbol(".".to_string())
+                },
+                Point {
+                    x: 0,
+                    y: 0,
+                    point_type: PointType::Symbol(".".to_string())
+                },
+                Point {
+                    x: 0,
+                    y: 0,
+                    point_type: PointType::Symbol("1".to_string())
+                },
+                Point {
+                    x: 0,
+                    y: 0,
+                    point_type: PointType::Symbol("1".to_string())
+                },
+                Point {
+                    x: 0,
+                    y: 0,
+                    point_type: PointType::Symbol("4".to_string())
+                },
+                Point {
+                    x: 0,
+                    y: 0,
+                    point_type: PointType::Symbol(".".to_string())
+                },
+                Point {
+                    x: 0,
+                    y: 0,
+                    point_type: PointType::Symbol(".".to_string())
+                },
+            ];
+        let result = parse_schematic_line(line);
+        assert_eq!(expected, result);
+    }
     
     #[test]
     fn should_identify_symbols() {}
