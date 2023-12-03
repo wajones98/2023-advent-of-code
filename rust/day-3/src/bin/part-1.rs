a3c9ec327856ed6a01a95fdf7cd4206f6f346b6d
Will <w.a.jones1998@gmail.com>
2023-12-03 13:58:10 +0000

    Scaffold tests

--- rust/day-3/src/bin/part-1.rs
+++ rust/day-3/src/bin/part-1.rs
@@ -2,3 +2,31 @@ fn main() {
     println!("Hello, world!");
 }
 
+#[cfg(test)]
+mod tests {
+    const TEST_ENGINE_SCHEMATIC: [&str; 10] = [
+        "467..114..",
+        "...*......",
+        "..35..633.",
+        "......#...",
+        "617*......",
+        ".....+.58.",
+        "..592.....",
+        "......755.",
+        "...$.*....",
+        ".664.598..",
+    ];
+
+
+    #[test]
+    fn should_parse_line() {}
+    
+    #[test]
+    fn should_identify_symbols() {}
+
+    #[test]
+    fn should_identify_numbers () {}
+
+    #[test]
+    fn should_calculate_total() {}
+}
