88cec9a9bd8a89f0f239370c646ff4d103e2f8f6
Will <w.a.jones1998@gmail.com>
2023-12-03 12:35:16 +0000

    Test for adding total of valid games

--- rust/day-2/src/bin/part-1.rs
+++ rust/day-2/src/bin/part-1.rs
@@ -75,7 +75,7 @@ fn validate_sets(expected_set: Set, revealed_sets: Vec<Set>) -> bool {
 
 #[cfg(test)]
 mod tests {
-    use crate::{Set, validate_sets};
+    use crate::{Set, validate_sets, Game};
     
     const VALID_SET: Set = Set {
         red: 12,
@@ -156,6 +156,25 @@ mod tests {
 
     #[test]
     fn should_correctly_total_valid_sets() {
+        let lines = vec![
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        ]; 
+        let mut total = 0;        
+        let games = lines.into_iter().map(|line| {
+            Game::parse_string(line)
+        });  
 
+        for game in games {
+            let valid = validate_sets(VALID_SET, game.sets_revealed);
+            if valid {
+                total = total + game.id;
+            }
+        }
+
+        assert_eq!(8, total);
     }
 }
