13e85874202b3ee2e0989d89a184cef5ed21af95
Will <w.a.jones1998@gmail.com>
2023-12-09 18:33:00 +0000

    test for parsing a single schematic line

--- rust/day-3/src/bin/part-1/schematic.rs
+++ rust/day-3/src/bin/part-1/schematic.rs
@@ -1,8 +1,66 @@
+use crate::point::{Point, PointType};
+
 pub type Schematic = Vec<Vec<Point>>;
 
-fn new_schematic(lines: Vec<&str>) -> Schematic {
+pub fn new_schematic(lines: Vec<&str>) -> Schematic {
     lines.iter().enumerate().map(|(i, line)| {
         parse_schematic_line(i, line)
     }).collect()
 }
 
+pub fn parse_schematic_line(y: usize, line: &str) -> Vec<Point> {
+    line.chars().enumerate().map(|(x, c)| {
+        let point_type: PointType = match c.to_digit(10) {
+            Some(digit) => PointType::Digit(digit),
+            None => PointType::Symbol(c.to_string()),
+        };
+        
+        Point {
+            x: (x as u32),
+            y: (y as u32),
+            point_type,
+        }
+    }).collect() 
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::point::{Point, PointType};
+
+    use super::parse_schematic_line;
+    
+    #[test]
+    fn it_parses_line() {
+        let line = "4.51.";
+        let expected = vec![
+            Point {
+                x: 0,
+                y: 0,
+                point_type: PointType::Digit(4),
+            },
+            Point {
+                x: 1,
+                y: 0,
+                point_type: PointType::Symbol(".".to_string()),
+            },
+            Point {
+                x: 2,
+                y: 0,
+                point_type: PointType::Digit(5),
+            },
+            Point {
+                x: 3,
+                y: 0,
+                point_type: PointType::Digit(1),
+            },
+            Point {
+                x: 4,
+                y: 0,
+                point_type: PointType::Symbol(".".to_string()),
+            },
+        ];       
+        let result = parse_schematic_line(0, line);
+
+        assert_eq!(expected, result);
+    }
+}
