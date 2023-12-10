289c70d2a409583648bf3b7b97ea08926d05cead
Will <w.a.jones1998@gmail.com>
2023-12-10 18:48:59 +0000

    Test matching numbers

--- rust/day-4/src/bin/part-1/main.rs
+++ rust/day-4/src/bin/part-1/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn main() {
     println!("Hello world");
 }
@@ -5,11 +7,17 @@ fn main() {
 #[derive(Debug, PartialEq)]
 struct Card {
     id: u32,
-    winning_numbers: Vec<u32>,
-    numbers: Vec<u32>,
+    winning_numbers: HashSet<u32>,
+    numbers: HashSet<u32>,
 }
 
-fn parse_line(line: &str) -> Card {
+impl Card {
+    fn matching_numbers(self) -> Vec<u32> {
+       self.winning_numbers.intersection(&self.numbers).cloned().collect()  
+    }
+}
+
+fn parse_line(line: &str) -> Card { 
     let parts: Vec<&str> = line.split(":").collect();
     
     let id = parts[0].split_whitespace().collect::<Vec<&str>>()[1];
@@ -17,8 +25,8 @@ fn parse_line(line: &str) -> Card {
 
     let parts: Vec<&str> = parts[1].split("|").collect();
 
-    let winning_numbers: Vec<u32> = parts[0].split_whitespace().map(|number| number.parse().unwrap()).collect();
-    let numbers: Vec<u32> = parts[1].split_whitespace().map(|number| number.parse().unwrap()).collect();
+    let winning_numbers: HashSet<u32> = parts[0].split_whitespace().map(|number| number.parse().unwrap()).collect();
+    let numbers: HashSet<u32> = parts[1].split_whitespace().map(|number| number.parse().unwrap()).collect();
     
     Card {
         id,
@@ -34,14 +42,34 @@ mod tests {
     #[test]
     fn it_parses_line() {
         let line = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
+        
+        let winning_numbers: HashSet<u32> = vec![41, 48, 83, 86, 17].into_iter().collect(); 
+        let numbers: HashSet<u32> = vec![83, 86, 6, 31, 17, 9, 48, 53].into_iter().collect(); 
 
         let expected = Card {
             id: 1,
-            winning_numbers: vec![41, 48, 83, 86, 17],
-            numbers: vec![83, 86, 6, 31, 17, 9, 48, 53],
+            winning_numbers,
+            numbers,
         };
 
         let result = parse_line(line);
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn it_gets_matching_numbers() {
+        let winning_numbers: HashSet<u32> = vec![41, 48, 83, 86, 17].into_iter().collect(); 
+        let numbers: HashSet<u32> = vec![83, 86, 6, 31, 17, 9, 48, 53].into_iter().collect(); 
+
+        let card = Card {
+            id: 1,
+            winning_numbers,
+            numbers,
+        };
+        
+        let expected = vec![48, 83, 17, 86].sort();
+        let result = card.matching_numbers().sort();
 
         assert_eq!(expected, result);
     }
