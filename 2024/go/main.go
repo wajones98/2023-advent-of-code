a5dec6cd1f77322baf1d1140ef1a89e4f6568ff7
Will <w.a.jones1998@gmail.com>
2024-12-02 20:41:40 +0000

    Add day2 to cli

--- 2024/go/main.go
+++ 2024/go/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/wajones98/advent-of-code/days/day1"
 	"log"
 	"os"
 	"strconv"
+
+	"github.com/wajones98/advent-of-code/days/day1"
+	"github.com/wajones98/advent-of-code/days/day2"
 )
 
 func main() {
@@ -28,6 +30,12 @@ func main() {
 		fmt.Printf("Part 1 Result: %d\n", result.Part1)
 		fmt.Printf("Part 2 Result: %d\n", result.Part2)
 	case 2:
+		result, err := day2.Run(arg)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Part 1 Result: %d\n", result.Part1)
+		fmt.Printf("Part 2 Result: %d\n", result.Part2)
 
 	}
 }
