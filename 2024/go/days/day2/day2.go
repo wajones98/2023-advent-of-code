3284665c355976cd5fa40cecf3bae5d1f1e3c55a
Will <w.a.jones1998@gmail.com>
2024-12-02 20:41:09 +0000

    Load file

--- 2024/go/days/day2/day2.go
+++ 2024/go/days/day2/day2.go
@@ -1,10 +1,39 @@
 package day2
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/wajones98/advent-of-code/input"
 
 	"github.com/wajones98/advent-of-code/days"
 )
+
+func Run(day int) (*days.Result[int, int], error) {
+	s, closeFile, err := input.GetInput(day)
+	if err != nil {
+		return nil, err
+	}
+	defer closeFile()
+
+	reports := []uint64{}
+
+	for s.Scan() {
+		line := s.Text()
+		for _, val := range strings.Split(line, " ") {
+			num, err := strconv.ParseUint(val, 10, 0)
+			if err != nil {
+				return nil, err
+			}
+			reports = append(reports, num)
+		}
+	}
+
+	return &days.Result[int, int]{
+		Part1: Part1(),
+		Part2: Part2(),
+	}, nil
+}
 
 func Part1() int {
 	return 0
