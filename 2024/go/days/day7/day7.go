b5e90144f47feaceaf0eb30c6a7ac959f4d355df
Will <w.a.jones1998@gmail.com>
2024-12-07 12:10:30 +0000

    LoadInput

--- 2024/go/days/day7/day7.go
+++ 2024/go/days/day7/day7.go
@@ -1,6 +1,10 @@
 package day7
 
 import (
+	"bufio"
+	"strconv"
+	"strings"
+
 	"github.com/wajones98/advent-of-code/input"
 
 	"github.com/wajones98/advent-of-code/days"
@@ -43,4 +47,41 @@ func Part2() (int, error) {
 	defer closeFile()
 
 	return 0, nil
+}
+
+var Combinations map[int][]string = map[int][]string{}
+
+type Equation struct {
+	Result int
+	Values []int
+}
+
+func LoadInput(s *bufio.Scanner) ([]Equation, error) {
+	equations := []Equation{}
+	for s.Scan() {
+		line := s.Text()
+		parts := strings.Split(line, ":")
+		result, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return nil, err
+		}
+
+		values := []int{}
+		valuesString := strings.Split(parts[1], " ")
+		for _, v := range valuesString {
+			value, err := strconv.Atoi(v)
+			if err != nil {
+				return nil, err
+			}
+
+			values = append(values, value)
+		}
+
+		equations = append(equations, Equation{
+			Result: result,
+			Values: values,
+		})
+	}
+
+	return equations, nil
 }
--- 2024/go/days/day7/day7_test.go
+++ 2024/go/days/day7/day7_test.go
@@ -1,1 +1,65 @@
 package day7
+
+import (
+	"bufio"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+const Input string = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`
+
+var Data []Equation = []Equation{
+	{
+		Result: 3267,
+		Values: []int{81, 40, 27},
+	},
+	{
+		Result: 83,
+		Values: []int{17, 5},
+	},
+	{
+		Result: 156,
+		Values: []int{15, 6},
+	},
+	{
+		Result: 7290,
+		Values: []int{68615},
+	},
+	{
+		Result: 161011,
+		Values: []int{16, 10, 13},
+	},
+	{
+		Result: 192,
+		Values: []int{17, 8, 14},
+	},
+	{
+		Result: 21037,
+		Values: []int{9, 7, 18, 13},
+	},
+	{
+		Result: 292,
+		Values: []int{11, 6, 16, 20},
+	},
+}
+
+func TestLoadInput(t *testing.T) {
+	s := bufio.NewScanner(strings.NewReader(Input))
+	actual, err := LoadInput(s)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(Data, actual) {
+		t.Errorf("Expected: %v\nGot: %v\n", Data, actual)
+	}
+}
