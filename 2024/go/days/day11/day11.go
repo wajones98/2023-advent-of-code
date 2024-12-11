c4918583a367de919f61bd8122fbc5bc1cca445d
Will <w.a.jones1998@gmail.com>
2024-12-11 17:03:13 +0000

    Check stone trasform rules

--- 2024/go/days/day11/day11.go
+++ 2024/go/days/day11/day11.go
@@ -61,3 +61,23 @@ func LoadInput(s *bufio.Scanner) ([]int, error) {
 	}
 	return values, nil
 }
+
+func Blink() {}
+
+func TransformStone(stone int) []int {
+	stones := []int{}
+	length := len(strconv.Itoa(stone))
+	switch {
+	case stone == 0:
+		stones = append(stones, 1)
+	case length%2 == 0:
+		stoneString := strconv.Itoa(stone)
+		first, _ := strconv.Atoi(stoneString[:length/2])
+		second, _ := strconv.Atoi(stoneString[length/2:])
+		stones = append(stones, first, second)
+	default:
+		stones = append(stones, stone*2024)
+	}
+
+	return stones
+}
--- 2024/go/days/day11/day11_test.go
+++ 2024/go/days/day11/day11_test.go
@@ -2,6 +2,7 @@ package day11
 
 import (
 	"bufio"
+	"fmt"
 	"reflect"
 	"strings"
 	"testing"
@@ -20,5 +21,27 @@ func TestLoadInput(t *testing.T) {
 
 	if !reflect.DeepEqual(Data, actual) {
 		t.Errorf("Expected: %v, Actual: %v\n", Data, actual)
+	}
+}
+
+func TestTransformStone(t *testing.T) {
+	tests := []struct {
+		Stone    int
+		Expected []int
+	}{
+		{0, []int{1}},
+		{1, []int{2024}},
+		{10, []int{1, 0}},
+		{99, []int{9, 9}},
+		{999, []int{2021976}},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%d", test.Stone), func(t *testing.T) {
+			actual := TransformStone(test.Stone)
+			if !reflect.DeepEqual(test.Expected, actual) {
+				t.Errorf("Expected: %v, Actual: %v\n", test.Expected, actual)
+			}
+		})
 	}
 }
