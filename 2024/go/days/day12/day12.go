718fb80dbe107041739f8c4006286b9b01e8b1ce
wajones98 <will.jones@byzgen.com>
2024-12-12 15:17:26 +0000

    scaffold day12

---
+++ 2024/go/days/day12/day12.go
@@ -0,0 +1,71 @@
+package day12
+
+import (
+	"bufio"
+	"strings"
+
+	"github.com/wajones98/advent-of-code/common"
+	"github.com/wajones98/advent-of-code/days"
+	"github.com/wajones98/advent-of-code/input"
+)
+
+const Day int = 12
+
+func Run() (*days.Result[int, int], error) {
+	pOne, err := Part1()
+	if err != nil {
+		return nil, err
+	}
+
+	pTwo, err := Part2()
+	if err != nil {
+		return nil, err
+	}
+
+	return &days.Result[int, int]{
+		Part1: pOne,
+		Part2: pTwo,
+	}, nil
+}
+
+func Part1() (int, error) {
+	_, closeFile, err := input.GetInput(Day)
+	if err != nil {
+		return 0, err
+	}
+	defer closeFile()
+
+	return 0, nil
+}
+
+func Part2() (int, error) {
+	_, closeFile, err := input.GetInput(Day)
+	if err != nil {
+		return 0, err
+	}
+	defer closeFile()
+
+	return 0, nil
+}
+
+func LoadInput(s *bufio.Scanner) (*common.TwoDMap[string], error) {
+	lines := []string{}
+	for s.Scan() {
+		lines = append(lines, s.Text())
+	}
+
+	width, height := len(lines[0]), len(lines)
+	twoDMap := common.NewTwoDMap[string](width, height)
+
+	for y, line := range lines {
+		chars := strings.Split(line, "")
+		for x, char := range chars {
+			err := twoDMap.Put(x, y, char)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return twoDMap, nil
+}
---
+++ 2024/go/days/day12/day12_test.go
@@ -0,0 +1,33 @@
+package day12
+
+import (
+	"bufio"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/wajones98/advent-of-code/common"
+)
+
+const Input = `AAAA
+BBCD
+BBCC
+EEEC`
+
+var Data = common.TwoDMap[string]{
+	Width:  4,
+	Height: 4,
+	Map:    []string{"A", "A", "A", "A", "B", "B", "C", "D", "B", "B", "C", "C", "E", "E", "E", "C"},
+}
+
+func TestLoadInput(t *testing.T) {
+	s := bufio.NewScanner(strings.NewReader(Input))
+	actual, err := LoadInput(s)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(Data, actual) {
+		t.Errorf("Expected: %v, Actual: %v\n", Data, actual)
+	}
+}
