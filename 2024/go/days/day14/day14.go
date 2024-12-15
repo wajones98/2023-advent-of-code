45d80a17fc9034818be22c0e7cfb4ad870f9644c
Will <w.a.jones1998@gmail.com>
2024-12-15 14:29:31 +0000

    Tests for loading input

--- 2024/go/days/day14/day14.go
+++ 2024/go/days/day14/day14.go
@@ -2,12 +2,15 @@ package day14
 
 import (
 	"bufio"
+	"strconv"
+	"strings"
 
+	"github.com/wajones98/advent-of-code/common"
 	"github.com/wajones98/advent-of-code/days"
 	"github.com/wajones98/advent-of-code/input"
 )
 
-const Day int = 14 
+const Day int = 14
 
 func Run() (*days.Result[int, int], error) {
 	pOne, err := Part1()
@@ -46,5 +49,48 @@ func Part2() (int, error) {
 	return 0, nil
 }
 
-func LoadInput(s *bufio.Scanner) (_, error) {
+type Robot struct {
+	X, Y int
+}
+
+func LoadInput(s *bufio.Scanner, width, height int) (*common.TwoDMap[[]Robot], error) {
+	twoDMap := common.NewTwoDMap[[]Robot](width, height)
+
+	for s.Scan() {
+		pv := strings.Split(s.Text(), " ")
+
+		p := strings.Split(strings.ReplaceAll(pv[0], "p=", ""), ",")
+		px, err := strconv.Atoi(p[0])
+		if err != nil {
+			return nil, err
+		}
+		py, err := strconv.Atoi(p[1])
+		if err != nil {
+			return nil, err
+		}
+
+		v := strings.Split(strings.ReplaceAll(pv[1], "v=", ""), ",")
+		vx, err := strconv.Atoi(v[0])
+		if err != nil {
+			return nil, err
+		}
+
+		vy, err := strconv.Atoi(v[1])
+		if err != nil {
+			return nil, err
+		}
+
+		r, err := twoDMap.Get(px, py)
+		if err != nil {
+			return nil, err
+		}
+		r = append(r, Robot{vx, vy})
+
+		err = twoDMap.Put(px, py, r)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return twoDMap, nil
 }
--- 2024/go/days/day14/day14_test.go
+++ 2024/go/days/day14/day14_test.go
@@ -5,19 +5,59 @@ import (
 	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/wajones98/advent-of-code/common"
 )
 
-const Input = ""
-const Data = ""
+const Input = `p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3`
+
+var Data = common.TwoDMap[[]Robot]{
+	Width:  11,
+	Height: 7,
+	Map: [][]Robot{
+		{{1, 3}}, {}, {{2, -1}}, {{-2, -2}, {-1, -2}}, {}, {}, {}, {}, {}, {}, {},
+		{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
+		{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
+		{}, {}, {}, {}, {}, {}, {{-1, -3}}, {{-1, 2}}, {}, {{2, 3}}, {{-1, 2}},
+		{{3, -3}}, {}, {{2, -3}}, {}, {}, {}, {}, {}, {}, {}, {},
+		{}, {}, {}, {}, {}, {}, {}, {}, {}, {{-3, -3}}, {},
+		{}, {}, {}, {}, {}, {}, {}, {{-1, -3}}, {}, {}, {},
+	},
+}
 
 func TestLoadInput(t *testing.T) {
 	s := bufio.NewScanner(strings.NewReader(Input))
-	actual, err := LoadInput(s)
+	actual, err := LoadInput(s, 11, 7)
 	if err != nil {
 		t.Error(err)
 	}
 
-	if !reflect.DeepEqual(Data, *actual) {
-		t.Errorf("Expected: %v, Actual: %v\\n", Data, actual)
+	if Data.Width != actual.Width {
+		t.Errorf("\nExpected: %v\nActual: %v\n", Data.Width, actual.Width)
+	}
+
+	if Data.Height != actual.Height {
+		t.Errorf("\nExpected: %v\nActual: %v\n", Data.Height, actual.Height)
+	}
+
+	if len(Data.Map) != len(actual.Map) {
+		t.Errorf("\nExpected: %v\nActual: %v\n", len(Data.Map), len(actual.Map))
+	}
+
+	for i, v := range actual.Map {
+		if len(v) != len(Data.Map[i]) && !reflect.DeepEqual(Data.Map[i], v) {
+			t.Errorf("\nExpected: %v\nActual: %v\n", Data.Map[i], v)
+		}
 	}
 }
