b4ea8a84463cc09067e916714371d557224fa991
Will <w.a.jones1998@gmail.com>
2024-12-15 21:16:17 +0000

    Load data

--- 2024/go/days/day15/day15_test.go
+++ 2024/go/days/day15/day15_test.go
@@ -20,31 +20,33 @@ const Input = `########
 
 <^^>>>vv<v>>v<<`
 
-var TwoDMap = &common.TwoDMap[Tile]{
-	Width:  8,
-	Height: 8,
-	Map: []Tile{
-		Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall,
-		Wall, Empty, Empty, Box, Empty, Box, Empty, Wall,
-		Wall, Wall, Robot, Empty, Box, Empty, Empty, Wall,
-		Wall, Empty, Empty, Empty, Box, Empty, Empty, Wall,
-		Wall, Empty, Wall, Empty, Box, Empty, Empty, Wall,
-		Wall, Empty, Empty, Empty, Box, Empty, Empty, Wall,
-		Wall, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
-		Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall,
+var LoadedData = Data{
+	TwoDMap: &common.TwoDMap[Tile]{
+		Width:  8,
+		Height: 8,
+		Map: []Tile{
+			Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall,
+			Wall, Empty, Empty, Box, Empty, Box, Empty, Wall,
+			Wall, Wall, Robot, Empty, Box, Empty, Empty, Wall,
+			Wall, Empty, Empty, Empty, Box, Empty, Empty, Wall,
+			Wall, Empty, Wall, Empty, Box, Empty, Empty, Wall,
+			Wall, Empty, Empty, Empty, Box, Empty, Empty, Wall,
+			Wall, Empty, Empty, Empty, Empty, Empty, Empty, Wall,
+			Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall,
+		},
 	},
+	Robot: &Coords{2, 1},
+	Moves: []Move{Left, Up, Up, Right, Right, Right, Down, Down, Left, Down, Right, Right, Down, Left, Left},
 }
-
-const Moves = ""
 
 func TestLoadInput(t *testing.T) {
 	s := bufio.NewScanner(strings.NewReader(Input))
-	actual, _, err := LoadInput(s)
+	actual, err := LoadInput(s)
 	if err != nil {
 		t.Error(err)
 	}
 
-	if !reflect.DeepEqual(*TwoDMap, *actual) {
-		t.Errorf("\nExpected:\n%v\nActual:\n%v\n", TwoDMap, actual)
+	if !reflect.DeepEqual(*LoadedData.TwoDMap, *actual) {
+		t.Errorf("\nExpected:\n%v\nActual:\n%v\n", LoadedData.TwoDMap, actual)
 	}
 }
