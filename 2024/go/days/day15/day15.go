041d263441e057b198058b39251ceea83692bb8b
Will <w.a.jones1998@gmail.com>
2024-12-15 19:54:47 +0000

    Load input

--- 2024/go/days/day15/day15.go
+++ 2024/go/days/day15/day15.go
@@ -3,11 +3,12 @@ package day15
 import (
 	"bufio"
 
+	"github.com/wajones98/advent-of-code/common"
 	"github.com/wajones98/advent-of-code/days"
 	"github.com/wajones98/advent-of-code/input"
 )
 
-const Day int = 15 
+const Day int = 15
 
 func Run() (*days.Result[int, int], error) {
 	pOne, err := Part1()
@@ -46,5 +47,38 @@ func Part2() (int, error) {
 	return 0, nil
 }
 
-func LoadInput(s *bufio.Scanner) (_, error) {
+type Tile rune
+
+const (
+	Wall  Tile = '#'
+	Robot      = '@'
+	Box        = 'O'
+)
+
+func LoadInput(s *bufio.Scanner) (*common.TwoDMap[Tile], string, error) {
+	lines := []string{}
+	for s.Scan() {
+		line := s.Text()
+		if line == "" {
+			continue
+		}
+		lines = append(lines, line)
+	}
+
+	moves := lines[len(lines)-1]
+	lines = lines[:len(lines)-1]
+
+	width, height := len(lines[0]), len(lines)
+	twoDMap := common.NewTwoDMap[Tile](width, height)
+
+	for y, line := range lines {
+		for x, char := range line {
+			err := twoDMap.Put(x, y, Tile(char))
+			if err != nil {
+				return nil, "", err
+			}
+		}
+	}
+
+	return twoDMap, moves, nil
 }
--- 2024/go/days/day15/day15_test.go
+++ 2024/go/days/day15/day15_test.go
@@ -2,20 +2,42 @@ package day15
 
 import (
 	"bufio"
+	"fmt"
 	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/wajones98/advent-of-code/common"
 )
 
-const Input = ""
-const Data = ""
+const Input = `########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<`
+
+var TwoDMap = &common.TwoDMap[Tile]{
+	Width:  8,
+	Height: 8,
+	Map:    []Tile{Wall},
+}
+
+const Moves = ""
 
 func TestLoadInput(t *testing.T) {
 	s := bufio.NewScanner(strings.NewReader(Input))
-	actual, err := LoadInput(s)
+	actual, moves, err := LoadInput(s)
 	if err != nil {
 		t.Error(err)
 	}
+
+	fmt.Printf("Moves: %s\n", moves)
+	fmt.Printf("Map: %s\n", actual)
 
 	if !reflect.DeepEqual(Data, *actual) {
 		t.Errorf("Expected: %v, Actual: %v\\n", Data, actual)
