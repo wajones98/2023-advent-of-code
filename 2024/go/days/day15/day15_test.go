802dd89dd37f78138e907e03afe26c610d4ade0d
Will <w.a.jones1998@gmail.com>
2024-12-15 18:03:59 +0000

    Scaffold day15

---
+++ 2024/go/days/day15/day15_test.go
@@ -0,0 +1,23 @@
+package day15
+
+import (
+	"bufio"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+const Input = ""
+const Data = ""
+
+func TestLoadInput(t *testing.T) {
+	s := bufio.NewScanner(strings.NewReader(Input))
+	actual, err := LoadInput(s)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(Data, *actual) {
+		t.Errorf("Expected: %v, Actual: %v\\n", Data, actual)
+	}
+}
